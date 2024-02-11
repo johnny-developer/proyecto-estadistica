f2c9eda465fae9c7d466c5477d5992ccc9df6977
Jenn Cortés <127272259+jennpper@users.noreply.github.com>
2024-02-11 14:28:56 -0600

    Update bernoolly.py
    Bernoully Jennifer

--- bernoolly.py
+++ bernoolly.py
@@ -1,6 +1,26 @@
-def bernoolly(p, q, n, k):
-    FOR I IN RANGE(10, 0, 1):
-        
+def calcular_probabilidad_bernuolli(p, n, k):
+    q = 1 - p
+    
+    def factorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * factorial(n-1)
 
+    def combinacion(n, k):
+        return factorial(n) / (factorial(k) * factorial(n - k))
 
-    return f'La P({x1} <= X <= {x2}) es: {probabilidad}'
+    def bernoulli(p, q, n, k):
+        return combinacion(n, k) * (p ** k) * (q ** (n - k))
+
+    if p < 0 or p > 1:
+        print("La probabilidad debe estar en el rango [0, 1].")
+        return None
+    
+    if k < 0 or k > n:
+        print("El número de éxitos debe estar en el rango [0, n].")
+        return None
+    
+    probability = bernoulli(p, q, n, k)
+    
+    return probability
